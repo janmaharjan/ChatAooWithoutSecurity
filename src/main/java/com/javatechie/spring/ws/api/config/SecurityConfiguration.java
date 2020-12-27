0ef7bf26660ecf019eb3cb2d8dd3ee19da35182e
Jan Maharjan <7558jan@gmail.com>
2020-12-27 10:18:37 +0545

    hh

---
+++ src/main/java/com/javatechie/spring/ws/api/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javatechie.spring.ws.api.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ *
+ * @author Maharjan
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class SecurityConfiguration implements WebSocketMessageBrokerConfigurer{
+
+    SecurityConfiguration(){
+    }
+  
+    
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/jan").withSockJS();
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+    
+} 
+
